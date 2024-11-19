a0f73210d045508e88ca2a601c680f811f0d4570
ITsyuryupa <openingthedoor@yandex.ru>
2024-11-19 15:50:36 +0400

    add controller and service

---
+++ src/main/java/itsyuryupa/mvclibrary/controller/AuthorController.java
@@ -0,0 +1,50 @@
+package itsyuryupa.mvclibrary.controller;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import itsyuryupa.mvclibrary.model.Author;
+import itsyuryupa.mvclibrary.model.Views;
+import itsyuryupa.mvclibrary.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorController {
+
+    @Autowired
+    private AuthorService authorService;
+
+    @JsonView(Views.BookSummary.class)
+    @GetMapping
+    public ResponseEntity<List<Author>> getAllAuthors() {
+        return ResponseEntity.ok(authorService.getAllAuthors());
+    }
+
+    @JsonView(Views.BookDetail.class)
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable Long id) {
+        Optional<Author> author = authorService.getAuthorById(id);
+        return author.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Author> createAuthor(@RequestBody Author author) {
+        Author savedAuthor = authorService.saveAuthor(author);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedAuthor);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
+        if (!authorService.getAuthorById(id).isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        authorService.deleteAuthor(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
---
+++ src/main/java/itsyuryupa/mvclibrary/controller/BookController.java
@@ -0,0 +1,61 @@
+package itsyuryupa.mvclibrary.controller;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import itsyuryupa.mvclibrary.model.Book;
+import itsyuryupa.mvclibrary.model.Views;
+import itsyuryupa.mvclibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public ResponseEntity<Page<Book>> getAllBooks(Pageable pageable) {
+        Page<Book> books = bookService.getAllBooks(pageable);
+        return ResponseEntity.ok(books);
+    }
+
+    @JsonView(Views.BookSummary.class)
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Optional<Book> book = bookService.getBookById(id);
+        return book.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book savedBook = bookService.saveBook(book);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedBook);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        if (!bookService.getBookById(id).isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        book.setId(id);
+        Book updatedBook = bookService.saveBook(book);
+        return ResponseEntity.ok(updatedBook);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        if (!bookService.getBookById(id).isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+

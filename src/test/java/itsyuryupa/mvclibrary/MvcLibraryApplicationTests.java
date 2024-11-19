d16cfe959b3be8460bb734e8a876c25f36cec411
ITsyuryupa <openingthedoor@yandex.ru>
2024-11-19 13:10:56 +0400

    init

---
+++ src/test/java/itsyuryupa/mvclibrary/MvcLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package itsyuryupa.mvclibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

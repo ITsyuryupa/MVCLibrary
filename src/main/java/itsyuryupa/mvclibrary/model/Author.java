bebbbb2ca8761ad47a5a211151d2630811e851de
ITsyuryupa <openingthedoor@yandex.ru>
2024-11-19 15:50:11 +0400

    add model

--- src/main/java/itsyuryupa/mvclibrary/model/Author.java
+++ src/main/java/itsyuryupa/mvclibrary/model/Author.java
@@ -1,5 +1,9 @@
 package itsyuryupa.mvclibrary.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.util.List;
@@ -8,14 +12,17 @@ import java.util.List;
 @Data
 public class Author {
 
+    @JsonView(Views.BookSummary.class)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonView(Views.BookSummary.class)
     private String name;
 
-    private String biography;
 
+    @JsonView(Views.BookDetail.class)
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
     private List<Book> books;
 }
--- src/main/java/itsyuryupa/mvclibrary/model/Book.java
+++ src/main/java/itsyuryupa/mvclibrary/model/Book.java
@@ -1,5 +1,8 @@
 package itsyuryupa.mvclibrary.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -7,17 +10,23 @@ import lombok.Data;
 @Data
 public class Book {
 
+    @JsonView(Views.BookSummary.class)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonView(Views.BookSummary.class)
     private String title;
 
+    @JsonView(Views.BookSummary.class)
     private String genre;
 
-    private int year;
+    @JsonView(Views.BookSummary.class)
+    private int published_year;
 
+    @JsonView(Views.BookDetail.class)
     @ManyToOne
     @JoinColumn(name = "author_id", nullable = false)
+    @JsonBackReference
     private Author author;
 }
